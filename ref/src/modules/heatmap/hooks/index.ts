a8d8dca90fc3a51001c7760f08c09e46ed8b3e73
archichudinow <156709410+archichudinow@users.noreply.github.com>
2026-01-23 16:20:44 +0000

    styling

--- ref/src/modules/heatmap/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Heatmap Module Hooks
- * Centralized exports for all heatmap hooks
- */
-
-export { useHeatmapInitialization } from './useHeatmapInitialization';
-export { useHeatmapAccumulation } from './useHeatmapAccumulation';
-export { useHeatmapEffects } from './useHeatmapEffects';
